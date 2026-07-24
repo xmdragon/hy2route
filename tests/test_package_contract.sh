7a78ff26be4e2d9540335d37c43f26596242c357
xmdragon <xm.dragon@gmail.com>
2026-07-24 04:44:46 -0700

    test: migrate runtime contracts to core

--- tests/test_package_contract.sh
+++ tests/test_package_contract.sh
@@ -1,27 +1,9 @@
 #!/bin/sh
 set -eu
-
-require_literal() {
-	file="$1"
-	literal="$2"
-	if ! grep -Fq "$literal" "$file"; then
-		echo "missing package contract in $file: $literal" >&2
-		exit 1
-	fi
-}
-
-require_literal files/etc/init.d/hy2route 'SUPERVISOR=/usr/libexec/hy2route/run-xray.sh'
-require_literal files/etc/init.d/hy2route 'GOMEMLIMIT=80MiB'
-require_literal files/etc/init.d/hy2route 'procd_set_param command "$SUPERVISOR" "$RUNDIR/xray.json" "$test_port"'
-require_literal files/usr/libexec/hy2route/generate.uc "keepAlivePeriod: number(relay.keep_alive_period, 0, 0, 60, 'keep_alive_period')"
-require_literal files/etc/config/hy2route "option keep_alive_period '0'"
-require_literal files/www/luci-static/resources/view/hy2route/main.js "o.datatype = 'range(0,60)';"
-require_literal files/www/luci-static/resources/view/hy2route/main.js "o.default = '0';"
-require_literal files/www/luci-static/resources/view/hy2route/main.js "0 表示关闭主动保活"
-require_literal Makefile 'PKG_RELEASE:=13'
-require_literal files/etc/config/hy2route "option smart_dns '1'"
-require_literal files/etc/config/hy2route "option smart_dns_port '65353'"
-require_literal Makefile 'files/usr/libexec/hy2route/run-xray.sh'
-require_literal Makefile '$(INSTALL_BIN) ./files/usr/libexec/hy2route/run-xray.sh $(1)/usr/libexec/hy2route/run-xray.sh'
-
+grep -Fq 'PKG_VERSION:=0.2.0' Makefile
+grep -Fq '$(INSTALL_BIN) ./build/hy2route-core' Makefile
+grep -Fq '$(INSTALL_DATA) ./build/hy2route-data.bin' Makefile
+! grep -F 'DEPENDS:=' Makefile | grep -Eq 'xray|chinadns'
+grep -Fq 'PROG=/usr/bin/hy2route-core' files/etc/init.d/hy2route
+! grep -Eq 'XRAY|CHINADNS|run-xray' files/etc/init.d/hy2route
 echo 'package contract tests passed'
